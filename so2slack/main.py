5bdd1ef1eeb849cbd0369dcd03055ae369932f52
Jan Krepl <jankrepl@yahoo.com>
2019-04-21 23:57:28 +0200

    finish preliminary version

--- so2slack/main.py
+++ so2slack/main.py
@@ -1,10 +1,17 @@
+from argparse import ArgumentParser
+from collections import deque
 import datetime  # noqa
 import os
 import time  # noqa
 
+import requests
+
 from slackclient import SlackClient
 
-TAG_TO_CHANNEL = {'python': 'python',
+TAG_TO_CHANNEL = {'deep-learning': 'deeplearning',
+                  'keras': 'keras',
+                  'pandas': 'pandas',
+                  'python': 'python',
                   'scikit-learn': 'scikit-learn'}
 
 
@@ -16,11 +23,14 @@ class Updater:
     tag : str
         Tag to monitor.
 
-    update_frequency_mins : int
+    freq : int
         Number of minutes between two queries.
 
     illegal_tags : None or list
         If a list, then questions that contain at least one of these illegal tags are not considered.
+
+    bot_name : str
+        Name of the slack bot.
 
 
     Attributes
@@ -41,13 +51,17 @@ class Updater:
         If int then represents UNIX timestamp of the last time the stack overflow was checked. If None,
         we have not run the script yet.
 
+    memory : deque
+        Keeps ids of the most recent questions in order to prevent accidental duplicates.
+
     """
 
-    def __init__(self, tag='python', update_frequency_mins=5, illegal_tags=None):
-        # define attributes
+    def __init__(self, tag='python', freq=5, illegal_tags=None, bot_name='lazy_lizard'):
+        # Define attributes
         self.tag = tag
-        self.update_frequency_mins = update_frequency_mins
+        self.freq = freq
         self.illegal_tags = illegal_tags or []
+        self.bot_name = bot_name
 
         self.channel = TAG_TO_CHANNEL.get(tag, 'random')
 
@@ -56,15 +70,66 @@ class Updater:
 
         self.slack_client = SlackClient(token=self.slack_key)
 
-        self.last_update = None
+        self.last_update = int(time.mktime(datetime.datetime.now().timetuple()))
+
+        self.memory = deque(maxlen=20)
 
     def run(self):
         """Main method that runs an infinite loop."""
-        pass
+
+        while True:
+            last_update_dt = datetime.datetime.utcfromtimestamp(self.last_update)
+
+            # stack overflow
+            so_items = self._get_so()
+
+            if so_items:
+                print('At time {} there were {} new items'.format(last_update_dt, len(so_items)))
+                self._send_slack(so_items)
+
+            else:
+                print('At time {} there were no new items.'.format(last_update_dt))
+
+            time.sleep(int(self.freq * 60))
 
     def _get_so(self):
         """Get the latest stack overflow questions."""
-        pass
+
+        seconds_tol = 100  # sometimes slack times are off
+
+        url = 'http://api.stackexchange.com/2.2/questions/unanswered'
+
+        params = {'key': self.so_key,
+                  'site': 'stackoverflow',
+                  'sort': 'creation',
+                  'pagesize': 10,
+                  'tagged': self.tag}
+
+        res = requests.get(url, params=params)
+
+        try:
+            _temp = res.json()
+            items = _temp['items']
+            quota_remaining = _temp['quota_remaining']
+
+            print('API calls left: {}'.format(quota_remaining))
+
+            filtered_items = [it for it in items if (it['creation_date'] >= self.last_update - seconds_tol) and
+                              not (set(it['tags']) & set(self.illegal_tags)) and
+                              it['question_id'] not in self.memory]
+
+            self.memory.extend([it['question_id'] for it in filtered_items])
+
+            return filtered_items
+
+        except KeyError as e:
+            print('Something went wrong with the request...')
+            print(e)
+
+            return []
+
+        finally:
+            self.last_update = int(time.mktime(datetime.datetime.now().timetuple()))
 
     def _send_slack(self, items):
         """ Send a message to slack.
@@ -75,4 +140,28 @@ class Updater:
             A list of question dictionaries.
 
         """
-        pass
+
+        for it in items:
+            title = it['title']
+            link = it['link']
+
+            message = '{}\n{}'.format(title, link)
+
+            self.slack_client.api_call('chat.postMessage',
+                                       channel=self.channel,
+                                       text=message,
+                                       username=self.bot_name,
+                                       icon_emoji=':robot_face:')
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+
+    parser.add_argument("--tag", default='python', type=str)
+    parser.add_argument("--freq", default=1, type=float)
+
+    args = parser.parse_args()
+
+    updater = Updater(**vars(args))
+
+    updater.run()
---
+++ so2slack/tests/test_main.py
@@ -0,0 +1,3 @@
+import pytest
+
+from so2slack.main import Updater
